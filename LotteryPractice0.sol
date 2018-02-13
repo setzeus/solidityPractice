88871f8e0e6662e44c30f772d6601e5af9974560
setzeus <jnajera1917@gmail.com>
2018-02-13 01:30:39 -0500

    lottery practice continued

--- LotteryPractice0.sol
+++ LotteryPractice0.sol
@@ -1,12 +1,30 @@
 pragma solidity ^0.4.18;
 
+//16 person lottery that only unlocks once completely filled
+//start with list of available tickets that are number 1 - 16
+//users purchase a ticket, that is now removed from available tickets
+//a new array records, for users that send the right amount, the ticket number & address sent from
+//once all tickets are purchased, award an address, based on a pgrn, the total lottery amount
+
 contract LotteryPractice {
 
+    uint _totalTickets = 16;
+
+    event ticketInfo(
+            uint _ticketAmount,
+            bool _purchased,
+            address _owner
+        );
+
     event winningLotteryNumbers(
-            uint256 _secondLast,
             uint256 _last,
             bytes32 testHash,
             uint256 testNum
+        );
+
+    event testLoggingArrays(
+           address [] lotteryTickets,
+           uint _totalTicketsLeft
         );
 
     function LotteryPractice () {
@@ -18,11 +36,41 @@ contract LotteryPractice {
         // Shift left by 252 bits (leaving just 4 bits left), then shift back to the right
         uint256 last = (hashNum * 2 ** 252) / (2 ** 252);
 
-        // Second to last character alone
-        // Shift left by 248 bits (leaving 8 bits left), but then shift right by 252
-        uint256 secondLast = (hashNum * 2 ** 248) / (2 ** 252);
 
-        winningLotteryNumbers(secondLast,last,myHash,hashNum);
+        winningLotteryNumbers(last,myHash,hashNum);
+    }
+
+
+
+    struct lotteryTicket {
+        uint ticketAmount;
+        bool purchased;
+        address owner;
+    }
+
+    mapping(address => lotteryTicket) lotteryTickets;
+
+    address[] public allLotteryTickets;
+
+
+
+    function purchaseLotteryTicket(address _ticketBuyer) public payable returns(bool) {
+        var lotteryTicket = lotteryTickets[_ticketBuyer];
+        require(msg.value == .5 ether);
+
+
+        lotteryTicket.ticketAmount = msg.value;
+        lotteryTicket.owner = _ticketBuyer;
+        lotteryTicket.purchased = true;
+
+        allLotteryTickets.push(_ticketBuyer);
+        _totalTickets = _totalTickets - 1;
+
+        ticketInfo(lotteryTicket.ticketAmount,lotteryTicket.purchased,lotteryTicket.owner);
+        testLoggingArrays(allLotteryTickets,_totalTickets);
+
+
+        return lotteryTicket.purchased;
     }
 
 
